1d2362d736b7247ba12aa3dc14b7525a75005cd5
GUGUcomet <83127928+GUGUcomet@users.noreply.github.com>
2023-02-19 00:41:30 +0900

    오늘은 여기까지

--- src/App.svelte
+++ src/App.svelte
@@ -1,51 +1,97 @@
 <script>
-  import svelteLogo from "./assets/svelte.svg";
+  import mainLogo from "./assets/demo2.png";
   import Counter from "./lib/Counter.svelte";
   import Counters from "./lib/Counters.svelte";
 </script>
 
 <main>
-  <style class="scss">
-  </style>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src="./vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
+  <div class="wrap">
+    <header>
+      <div class="header-left">
+        <div class="header-menu" />
+        <a class="header-logo" href="/">
+          <!-- <img src={mainLogo} alt="로고" />
+           <img src="/src/assets/demo2.png" alt="로고" /> -->
+        </a>
+      </div>
+      <div class="header-toolbar">
+        <div class="header-toolbtn">
+          <img src="/src/assets/ico_search.svg" alt="검색" />
+        </div>
+        <div class="header-toolbtn">
+          <span class="bullet animation-blink" />
+          <img src="/src/assets/ico_search.svg" alt="검색" />
+        </div>
+        <div class="header-toolbtn">
+          <img src="/src/assets/ico_search.svg" alt="검색" />
+        </div>
+      </div>
+    </header>
+
+    <div class="card">
+      <Counter />
+      <Counters />
+    </div>
   </div>
-  <h1>Vite + Svelㅀddddddddㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎte</h1>
-
-  <div class="card">
-    <Counter />
-    <Counters />
-  </div>
-
-  <p>
-    Check out <a
-      href="https://github.com/sveltejs/kit#readme"
-      target="_blank"
-      rel="noreferrer">SvelteKit</a
-    >, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">Click on the Vite and Svelte logos to learn more</p>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
+  .wrap {
+    width: 100vw;
   }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
+  header {
+    display: flex;
+    /* justify-content: space-between; */
+    align-items: center;
+    height: 65px;
+    border-bottom: var(--bs-header-tablet-and-mobile-default-border);
+    background-color: #000;
+    padding: 0 15px;
   }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
+  .header-left {
+    flex-grow: 1;
   }
-  .read-the-docs {
-    color: #888;
+  .header-logo {
+    display: block;
+    height: 35px;
+    width: 35px;
+    padding: 10px;
+    background: url(/src/assets/demo2.png) 50% 50% no-repeat;
+    background-size: contain;
+  }
+  .header-toolbtn {
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    padding: 0;
+    width: 30px;
+    height: 30px;
+    border-radius: 5px;
+  }
+  .header-toolbtn {
+    position: relative;
+  }
+  .header-toolbtn span.bullet {
+    background-color: green;
+    width: 6px;
+    height: 6px;
+    border-radius: 100% !important;
+    position: absolute;
+    top: 0px;
+    left: 50%;
+    transform: translate(-50%, -50%) !important;
+  }
+  .header-toolbtn:hover {
+    background-color: rgba(255, 255, 255, 0.1);
+    cursor: pointer;
+  }
+  .animation-blink {
+    animation: animationBlink 1s steps(5, start) infinite;
+  }
+
+  @keyframes animationBlink {
+    to {
+      visibility: hidden;
+    }
   }
 </style>
